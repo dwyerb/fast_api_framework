6229990989c7639ce0baa67ee08e5ebe74d5a199
br003371 <brendan.dwyer@maxar.com>
2022-04-22 11:37:24 -0400

    Updated w/ path params

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -7,13 +8,21 @@ class ModelName(str, Enum):
     alexnet = 'alexnet'
     resnet = 'resnet'
     lenet = 'lenet'
+
+fake_db = [{'item_name':'foo'},{'item_name':'bar'},{'item_name':'kate'},{'item_name':'barry'}]
+
 @app.get("/")
 async  def root():
     return {"message":"get git"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return{"item id":item_id}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({'q':q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -24,3 +33,7 @@ async def get_model(model_name: ModelName):
             return {"model_name": model_name, "message": "LeCNN all the images"}
 
         return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/items/")
+async def read_item(skip: int =0, limit: int=10):
+    return fake_db[skip: skip + limit]
